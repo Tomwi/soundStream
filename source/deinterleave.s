0bcddf77a51f0ee6aa79420592956c555c178888
Tomwi <Tomwi@dev>
2012-06-08 16:15:51 +0200

    Added 8bit support (stereo and mono)

--- source/deinterleave.s
+++ source/deinterleave.s
@@ -45,3 +45,58 @@ _deInterleave:
 
 	bx lr
 
+
+.global _8bdeInterleave
+.hidden _8bdeInterleave
+.type _8bdeInterleave, %function
+
+.arm
+.align 2
+
+@ r0 = in
+@ r1 = out
+@ r2 = len
+
+_8bdeInterleave:
+
+
+cmp r2, #4
+bxlt lr   @ nothing to split
+bic r2, r2, #3
+push {r4-r9}
+
+mov ip, #0xFF
+mov r9, ip, lsl #24
+add r3, r1, #(BUF_BYTES/2)
+
+.loop:
+ldmia r0!,{r4-r5}
+@ links
+and r6, ip, r4, lsr #16
+orr r6, r6, r4, lsl #24
+
+and r7, ip, r5, lsr #16
+orr r7, r7, r5, lsl #24
+
+ror r7, r7, #8
+orr r7, r7, r6, ror #24
+
+str r7,[r1],#4
+
+@rechts
+and r6, r9, r4, lsl #16
+orr r6, r6, r4, lsr #24
+
+and r7, r9, r5, lsl #16
+orr r7, r7, r5, lsr #24
+
+ror r7, r7, #8
+orr r7, r7, r6, ror #24
+
+str r7,[r3], #4
+
+subs r2,#4
+bne .loop
+
+pop {r4-r9}
+bx lr
