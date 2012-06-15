d79cf82c8fb726ed05849d2ef7c81b23f5898f96
fincs <fernandoincs@hotmail.com>
2012-06-15 21:30:01 +0200

    Fix timer desync b0rk

--- arm7/source/main.c
+++ arm7/source/main.c
@@ -17,32 +17,39 @@ void FifoMsgHandler(int num_bytes, void *userdata)
 	switch(msg.type) {
 	case FIFO_AUDIO_START:
 		if(channels <=2 && channels > 0) {
-			int freq = msg.property & 0xFFFF;
+			int basetmr = 0x2000000 / (msg.property & 0xFFFF) &~ 1;
 			for(i=0; i<channels; i++) {
-				SCHANNEL_TIMER(i) = SOUND_FREQ(freq);
+				SCHANNEL_TIMER(i) = -(basetmr>>1); // Sound channels use half of the base timer
 				SCHANNEL_SOURCE(i) = (u32)(msg.buffer+msg.bufLen*i*bytSmp);
 				SCHANNEL_LENGTH(i) = (msg.bufLen*bytSmp)/4;	// length is counted in words, sample is halfword
 				SCHANNEL_REPEAT_POINT(i) = 0;
 			}
 
-			tmrData = -0x2000000 / freq;
+			tmrData = -basetmr;
 
 	case FIFO_AUDIO_RESUME:
 
-			TIMER_CR(0) = 0;
-			TIMER_CR(1) = 0;
-			TIMER_DATA(0) = tmrData;
-			TIMER_DATA(1) = 0;
-			
-			
-			if(channels == 2) {
-				SCHANNEL_CR(0) = SOUND_REPEAT|fmt|SCHANNEL_ENABLE|SOUND_VOL(127)|SOUND_PAN(0);
-				SCHANNEL_CR(1) = SOUND_REPEAT|fmt|SCHANNEL_ENABLE|SOUND_VOL(127)|SOUND_PAN(127);
-			} else
-				SCHANNEL_CR(0) = SOUND_REPEAT|fmt|SCHANNEL_ENABLE|SOUND_VOL(127)|SOUND_PAN(64);
+			{
+				int cS = enterCriticalSection();
 
-			TIMER_CR(0) = TIMER_ENABLE;
-			TIMER_CR(1) = TIMER_ENABLE | TIMER_CASCADE;
+				TIMER_CR(0) = 0;
+				TIMER_CR(1) = 0;
+				TIMER_DATA(0) = tmrData;
+				TIMER_DATA(1) = 0;
+			
+			
+				if(channels == 2) {
+					SCHANNEL_CR(0) = SOUND_REPEAT|fmt|SCHANNEL_ENABLE|SOUND_VOL(127)|SOUND_PAN(0);
+					SCHANNEL_CR(1) = SOUND_REPEAT|fmt|SCHANNEL_ENABLE|SOUND_VOL(127)|SOUND_PAN(127);
+				} else
+					SCHANNEL_CR(0) = SOUND_REPEAT|fmt|SCHANNEL_ENABLE|SOUND_VOL(127)|SOUND_PAN(64);
+
+				TIMER_CR(0) = TIMER_ENABLE;
+				TIMER_CR(1) = TIMER_ENABLE | TIMER_CASCADE;
+
+				leaveCriticalSection(cS);
+			}
+
 			fifoSendValue32 (fifoChan, 1);
 		}
 		
@@ -56,7 +63,7 @@ void FifoMsgHandler(int num_bytes, void *userdata)
 		break;
 
 	case FIFO_AUDIO_READTMR:
-		fifoSendValue32(fifoChan, (TIMER_DATA(1)-3)&0xFFFF);
+		fifoSendValue32(fifoChan, (TIMER_DATA(1)-16)&0xFFFF);
 		break;
 	}
 }
